94549988cde5c2533a5bc77d2dfa123228c09bac
wendelFacul <161367279+wendellastfacul@users.noreply.github.com>
2024-03-04 21:53:41 -0300

    saadsdd

---
+++ ceusus.java
@@ -0,0 +1,10 @@
+public class ceusus {
+    public static void main(String[] args) {
+    float ceusus = 18f;
+    double fire = ((9*ceusus)/5) + 32;
+    
+    System.out.println(fire);
+
+
+    }   
+}
---
+++ trimestre.java
@@ -0,0 +1,13 @@
+public class trimestre {
+    public static void main(String[] args) {
+        int gasto1 = 15000;
+        int gasto2 = 17000;
+        int gasto3 = 23000;
+
+        int total_gasto = (gasto1+gasto2+gasto3);
+        int media = total_gasto / 3;
+        System.out.print("O total: " + total_gasto);
+        System.out.print(" A media é : " + media );
+
+    }
+}
